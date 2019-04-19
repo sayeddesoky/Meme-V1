e85c54f3305151e7ee27a4667c6b8424f7e5d27e
Sayed <sayed@sayeds-macbook-pro.local>
2019-04-19 22:03:59 +0200

    Initial Commit

---
+++ pickimage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pickimage
+//
+//  Created by Sayed  on 4/19/19.
+//  Copyright © 2019 Sayed . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
